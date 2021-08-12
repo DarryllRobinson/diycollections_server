818109f9c39633095ab2da42239cde5026774cc0
Darryll Robinson <darryll@stillproud.com>
2021-08-12 18:03:15 +1000

    Starting pdfkit

--- src/crons/cron.invoices.js
+++ src/crons/cron.invoices.js
@@ -1,98 +1,10 @@
 const cron = require('node-cron');
 const fs = require('fs');
-const niceInvoice = require('nice-invoice');
+const PDFDocument = require('pdfkit');
 
 const db = require('../helpers/db');
 const sendEmail = require('../helpers/send-email');
 const createFolder = require('../helpers/create-folder');
-
-// Need to make the logo fetch dynamic for actual clients - this is for testing purposes
-//const mieLogo = require('../assets/img/mie_logo.png');
-//const mieLogo = fs.readFileSync('../assets/img/mie_logo.png');
-
-// Line items for testing invoice creation until API or flat file is available
-const invoiceLineItems = [
-  {
-    line: 10,
-    code: 5048,
-    description: 'Combined Credit Check - South Africa',
-    qty: 554,
-    unit: 'ea',
-    unitPrice: 9,
-  },
-  {
-    line: 20,
-    code: 9708,
-    description: 'Combined Sequestration',
-    qty: 136,
-    unit: 'ea',
-    unitPrice: 10,
-  },
-  {
-    line: 30,
-    code: 5133,
-    description: 'Compuscan Comprehensive - South Africa',
-    qty: 25,
-    unit: 'ea',
-    unitPrice: 15,
-  },
-  {
-    line: 40,
-    code: 5196,
-    description: 'Experian Comprehensive - South Africa',
-    qty: 84,
-    unit: 'ea',
-    unitPrice: 25,
-  },
-  {
-    line: 50,
-    code: 5197,
-    description: 'Experian Notices - South Africa',
-    qty: 35,
-    unit: 'ea',
-    unitPrice: 30,
-  },
-  {
-    line: 60,
-    code: 5426,
-    description: 'Sequestration - South Africa',
-    qty: 14,
-    unit: 'ea',
-    unitPrice: 17,
-  },
-  {
-    line: 70,
-    code: 5444,
-    description: 'Transunion Comprehensive - South Africa',
-    qty: 94,
-    unit: 'ea',
-    unitPrice: 16,
-  },
-  {
-    line: 80,
-    code: 5442,
-    description: 'Transunion Notices - South Africa',
-    qty: 39,
-    unit: 'ea',
-    unitPrice: 11,
-  },
-  {
-    line: 90,
-    code: 6008,
-    description: 'XDS Comprehensive - South Africa',
-    qty: 34,
-    unit: 'ea',
-    unitPrice: 5,
-  },
-  {
-    line: 100,
-    code: 9780,
-    description: 'AFIS Standard Resubmission',
-    qty: 14,
-    unit: 'ea',
-    unitPrice: 7,
-  },
-];
 
 setTimeout(() => {
   runInvoices();
@@ -103,17 +15,26 @@ async function runInvoices() {
   const records = await fetchRecords();
 
   // Loop through records
-  records.forEach(async (record) => {
+  records.forEach((record) => {
     //console.log(record.customerName);
-    let items = [];
-    invoiceLineItems.forEach((invoiceLineItem) => {
-      items.push(setItems(invoiceLineItem));
+    const info = {
+      Title: record.customerName + ' invoice',
+      Author: 'The System Collections Platform',
+      Subject: 'Invoice from MIE',
+    };
+
+    let doc = new PDFDocument({
+      font: 'Helvetica',
+      info,
+      margin: 40,
+      size: 'A4',
     });
 
-    const invoiceDetail = populateInvoiceVariables(record, items);
+    // Populate the document with various information and variables
+    header(doc, record);
 
     // Make a folder for the invoice
-    await createFolder(
+    createFolder(
       __dirname + `/invoices/${record.customerRefNo}`,
       0744,
       function (err) {
@@ -122,8 +43,9 @@ async function runInvoices() {
       }
     );
 
-    createInvoice(invoiceDetail, record);
+    createInvoice(doc, record);
     // create email with link to invoice
+    doc.end();
   });
   console.log(`Finished running ${records.length} invoices`);
 }
@@ -140,39 +62,30 @@ async function fetchRecords() {
       'address5',
       'f_clientId',
     ],
+    limit: 2,
   });
   return records;
 }
 
-function populateInvoiceVariables(record, items) {
-  return {
-    shipping: {
-      name: record.customerName,
-      address: record.address1,
-      city: record.address2,
-      state: record.address3,
-      country: record.address4,
-      postal_code: record.address5,
-    },
-    items: items,
-    subtotal: 156,
-    total: 156,
-    order_number: 1234222,
-    header: {
-      company_name: 'Managed Integrity Evaluation (MIE)',
-      company_logo: 'logo.png',
-      company_address:
-        'Building 2	| Jean Park Chambers 252 Jean Avenue Centurion 0157',
-    },
-    footer: {
-      text: 'Enquiries MIE Accounts Email accounts@mie.co.za',
-    },
-    currency_symbol: 'R',
-    date: {
-      billing_date: '11 August 2021',
-      due_date: '10 September 2021',
-    },
-  };
+function header(doc, record) {
+  doc
+    .fontSize(8)
+    .text(`INVOICE TO: ${record.customerName.substring(0, 15)}`, 30, 30)
+    .text(`DELIVER TO: ${record.customerName.substring(0, 15)}`, 180, 30)
+    .text(`Managed Integrity Evaluation (MIE)`, 390, 30)
+    .text(`XXXX Limited`, 30, 45)
+    .text(`XXXX Group`, 180, 45)
+    .text(`Building 2`, 390, 45)
+    .text(`P.O. Box 0000`, 30, 60)
+    .text(`P.O. Box 509741`, 180, 60)
+    .text(`Jean Park Chambers`, 390, 60)
+    .text(`Randburg`, 30, 75)
+    .text(`Randburg`, 180, 75)
+    .text(`252 Jean Avenue`, 390, 75)
+    .text(`2125`, 30, 90)
+    .text(`2125`, 180, 90)
+    .text(`Centurion`, 390, 90)
+    .text(`0157`, 390, 105);
 }
 
 function setItems(item) {
@@ -185,14 +98,15 @@ function setItems(item) {
   };
 }
 
-function createInvoice(invoiceDetail, record) {
+function createInvoice(doc, record) {
   //console.log('createInvoice: ', record.customerRefNo);
   // First remove forward slashes from the customer name so as not to break the folder structure
   const name = record.customerName.replace(/\//g, ' ');
   try {
-    niceInvoice(
-      invoiceDetail,
-      __dirname + `/invoices/${record.customerRefNo}/${name}.pdf`
+    doc.pipe(
+      fs.createWriteStream(
+        __dirname + `/invoices/${record.customerRefNo}/${name}.pdf`
+      )
     );
   } catch (err) {
     console.error(err);
